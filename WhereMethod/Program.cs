648d6049a4a8d71b5457675c176e04578a024794
Paul Hennessey <paul@paulhennessey.co.uk>
2015-02-03 09:02:49 +0000

    First commit.

---
+++ WhereMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereMethod
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Program().Run();
+        }
+
+        public void Run()
+        {         
+            var numbers = Enumerable.Range(0, Int32.MaxValue);
+
+            var filteredNumbers = numbers.Filter(i => (i % 2 != 0));
+
+            foreach (var number in filteredNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> numbers, Func<T, bool> del)
+        {
+            foreach (var number in numbers)
+            {
+                if (del(number))
+                    yield return number;
+            }
+        }
+    }
+}
+
---
+++ WhereMethod/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhereMethod.Solution
+{
+    class Solution
+    {
+        public static void Main2()
+        {
+            var nums = Enumerable.Range(0, Int32.MaxValue);
+
+            IEnumerable<int> filteredNums = nums.Filter(i => i % 2 == 0);
+
+            foreach (int i in filteredNums)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> nums, Func<T, bool> del)
+        {
+            foreach (T i in nums)
+            {
+                if (del(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
